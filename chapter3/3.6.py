ee44d3bb4608a9f0691cdbc6e5d161bcfb5dffcc
Yemdjih Kaze Nasser <kaze.nasser@outlook.com>
2019-02-22 06:27:47 +0100

    Adding comments to the code

--- chapter3/3.6.py
+++ chapter3/3.6.py
@@ -3,4 +3,6 @@ import math
 code = eval(input("Enter an ASCII coded: "))
 #character = ord(code)
 #area = (n * math.pow(s, 2)) / (4 * math.tan(math.pi / n))
+
+# Print the equivalent character of the given ASCII code
 print("The character is", chr(code))
